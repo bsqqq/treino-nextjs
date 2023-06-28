cc09c0906b989ec6bfc7bedc315f975e2b94cac9
Vinicius Moura Aragão <vinicius.aragao@serpro.gov.br>
2023-06-28 11:01:12 -0300

    resolvendo import esquecido na rota de filmes/[id]

--- pages/filme/[id].js
+++ pages/filme/[id].js
@@ -1,26 +1,34 @@
+import { api_base, api_key } from "../../lib/tmdb";
 
 export default function FilmeSelecionado({ json }) {
-    const { result } = json
+  const { result } = json;
 
-    return (
-        <>
-            <div style={{ padding: "20px" }}>
-                <img src={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} width={"900px"} style={{ borderRadius: "30px" }} />
-                <h1>{result.title} ({result.original_title})</h1>
-                <h3 style={{ maxWidth: "800px" }}>{result.overview}</h3>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div style={{ padding: "20px" }}>
+        <img
+          src={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+          width={"900px"}
+          style={{ borderRadius: "30px" }}
+        />
+        <h1>
+          {result.title} ({result.original_title})
+        </h1>
+        <h3 style={{ maxWidth: "800px" }}>{result.overview}</h3>
+      </div>
+    </>
+  );
 }
 
-
 export async function getServerSideProps(context) {
-    const filme = await fetch(`${api_base}/movie/${context.params.id}?api_key=${api_key}&language=pt-BR`)
-    const json = await filme.json()
-    // console.log("json", json)
-    return {
-        props: {
-            json
-        }
-    }
+  const filme = await fetch(
+    `${api_base}/movie/${context.params.id}?api_key=${api_key}&language=pt-BR`
+  );
+  const json = await filme.json();
+  // console.log("json", json)
+  return {
+    props: {
+      json,
+    },
+  };
 }
